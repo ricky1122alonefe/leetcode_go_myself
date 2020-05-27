df445b9cd2e5999c0fac10126251171ba8f9da36
ricky1122alonefe <zradelaide1122@163.com>
2020-05-27 16:37:38 +0800

    完成2简单题目

---
+++ to0/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(numberOfSteps(14))
+}
+
+func numberOfSteps(num int) int {
+	result :=0
+	for num>0{
+		if num %2 ==1 {
+			num = num -1
+		}else{
+			num = num/2
+		}
+		result = result+1
+	}
+	return result
+}
